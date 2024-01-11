9eec71426c54be11c70869f62635fa7ba8f3f5ad
Niall <niall@Aarons-MacBook-Air.local>
2024-01-11 17:18:47 +0000

    added textfields, button and basic level validation

--- app/src/main/java/com/niallmurph/noteapp/screens/NoteScreen.kt
+++ app/src/main/java/com/niallmurph/noteapp/screens/NoteScreen.kt
@@ -8,7 +8,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,10 +16,15 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.niallmurph.noteapp.R
+import com.niallmurph.noteapp.components.NoteButton
 import com.niallmurph.noteapp.components.NoteInputText
 
 @Composable
 fun NoteScreen() {
+
+    var title by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier
             .padding(6.dp)
@@ -29,7 +34,10 @@ fun NoteScreen() {
                 Text(text = stringResource(id = R.string.app_name))
             },
             actions = {
-                Icon(imageVector = Icons.Rounded.Notifications, contentDescription = "Top App Bar Icon")
+                Icon(
+                    imageVector = Icons.Rounded.Notifications,
+                    contentDescription = "Top App Bar Icon"
+                )
             },
             backgroundColor = Color.LightGray
         )
@@ -39,8 +47,29 @@ fun NoteScreen() {
             modifier = Modifier
                 .fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            NoteInputText(text = "", label = "Hello", onTextChange = {})
+        ) {
+            NoteInputText(
+                modifier = Modifier.padding(vertical = 8.dp),
+                text = title,
+                label = "Title",
+                onTextChange = {
+                    if (it.all { char ->
+                            char.isLetter() || char.isWhitespace()
+                        }) title = it
+                }
+            )
+            NoteInputText(
+                modifier = Modifier.padding(vertical = 8.dp),
+                text = description,
+                label = "Add a note",
+                onTextChange = {
+                    if (it.all { char ->
+                            char.isLetter() || char.isWhitespace()
+                        }) description = it
+                }
+            )
+            NoteButton(text = "Save", onClick = { /*TODO*/ })
+
         }
 
     }
