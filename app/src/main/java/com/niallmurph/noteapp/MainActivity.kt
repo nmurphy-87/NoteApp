5d46f68ecf0e4496003a1cb43809e7d591fea871
Niall <niall@Aarons-MacBook-Air.local>
2024-01-11 18:08:54 +0000

    Added dynamic addition and deletion of notes

--- app/src/main/java/com/niallmurph/noteapp/MainActivity.kt
+++ app/src/main/java/com/niallmurph/noteapp/MainActivity.kt
@@ -1,24 +1,41 @@
 package com.niallmurph.noteapp
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
+import com.niallmurph.noteapp.data.Note
 import com.niallmurph.noteapp.data.NotesDataSource
 import com.niallmurph.noteapp.screens.NoteScreen
 import com.niallmurph.noteapp.ui.theme.NoteAppTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NoteAppTheme {
-                NoteScreen(
-                    notes = NotesDataSource().loadNotes(),
-                    onAddNote = {},
-                    onRemoveNote = {}
-                )
+                Surface(color = MaterialTheme.colors.background) {
+                    val notes = remember {
+                        mutableStateListOf<Note>()
+                    }
+                    NoteScreen(
+                        notes = notes,
+                        onAddNote = {
+                            notes.add(it)
+                        },
+                        onRemoveNote = {
+                            notes.remove(it)
+                        }
+                    )
+                }
             }
         }
     }
