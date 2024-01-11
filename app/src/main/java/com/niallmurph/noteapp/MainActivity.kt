e1be13878f3df67d6b9a34f7b24bc6944a848bfb
Niall <niall@Aarons-MacBook-Air.local>
2024-01-11 17:32:24 +0000

    Added data class and dummy data

--- app/src/main/java/com/niallmurph/noteapp/MainActivity.kt
+++ app/src/main/java/com/niallmurph/noteapp/MainActivity.kt
@@ -3,12 +3,7 @@ package com.niallmurph.noteapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.niallmurph.noteapp.screens.NoteScreen
 import com.niallmurph.noteapp.ui.theme.NoteAppTheme
@@ -18,7 +13,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NoteAppTheme {
-                NoteScreen()
+                NoteScreen(
+                    notes = emptyList(),
+                    onAddNote = {},
+                    onRemoveNote = {}
+                )
             }
         }
     }
