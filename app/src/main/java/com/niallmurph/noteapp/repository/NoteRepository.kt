f5ac9f34b016a185a662f31f1f72ef1adc277c9a
Niall <niall@Aarons-MacBook-Air.local>
2024-01-12 14:05:13 +0000

    m

--- app/src/main/java/com/niallmurph/noteapp/repository/NoteRepository.kt
+++ app/src/main/java/com/niallmurph/noteapp/repository/NoteRepository.kt
@@ -11,15 +11,10 @@ import kotlinx.coroutines.flow.flowOn
 class NoteRepository @Inject constructor(private val noteDatabaseDao : NoteDatabaseDao) {
 
     suspend fun addNote(note : Note) = noteDatabaseDao.insert(note)
-
     suspend fun updateNote(note : Note) = noteDatabaseDao.update(note)
-
     suspend fun deleteNote(note : Note) = noteDatabaseDao.deleteNote(note)
-
     suspend fun deleteAllNotes() = noteDatabaseDao.deleteAllEntries()
-
     fun getAllNotes() : Flow<List<Note>> = noteDatabaseDao.getNotes().flowOn(Dispatchers.IO).conflate()
-
     suspend fun getNoteById(id : String) = noteDatabaseDao.getNoteById(id)
 
 }
