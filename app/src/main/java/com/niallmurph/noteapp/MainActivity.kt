17c115832e9d97ebe5f5a3d02dee5507ce38f60e
Niall <niall@Aarons-MacBook-Air.local>
2024-01-12 09:22:06 +0000

    added viewModel dependency and incorporated use of ViewModel into spp

--- app/src/main/java/com/niallmurph/noteapp/MainActivity.kt
+++ app/src/main/java/com/niallmurph/noteapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -11,9 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.niallmurph.noteapp.data.Note
 import com.niallmurph.noteapp.data.NotesDataSource
 import com.niallmurph.noteapp.screens.NoteScreen
+import com.niallmurph.noteapp.screens.NoteViewModel
 import com.niallmurph.noteapp.ui.theme.NoteAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,22 +26,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             NoteAppTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    val notes = remember {
-                        mutableStateListOf<Note>()
-                    }
-                    NoteScreen(
-                        notes = notes,
-                        onAddNote = {
-                            notes.add(it)
-                        },
-                        onRemoveNote = {
-                            notes.remove(it)
-                        }
-                    )
+                    val noteViewModel : NoteViewModel by viewModels()
+                    NotesApp(noteViewModel = noteViewModel)
                 }
             }
         }
     }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun NotesApp(noteViewModel: NoteViewModel = viewModel()){
+    val notesList = noteViewModel.getAllNotes()
+
+    NoteScreen(
+        notes = notesList,
+        onAddNote = { noteViewModel.addNote(it) },
+        onRemoveNote = { noteViewModel.removeNote(it) }
+    )
 }
 
 @Preview(showBackground = true)
