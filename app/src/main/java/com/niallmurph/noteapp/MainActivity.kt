b2d6bf0f00f329bea99eb362322e5ec49bf53753
Niall <niall@Aarons-MacBook-Air.local>
2024-01-12 10:15:45 +0000

    Setup the App Module and Note Application component

--- app/src/main/java/com/niallmurph/noteapp/MainActivity.kt
+++ app/src/main/java/com/niallmurph/noteapp/MainActivity.kt
@@ -9,16 +9,14 @@ import androidx.annotation.RequiresApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.niallmurph.noteapp.data.Note
-import com.niallmurph.noteapp.data.NotesDataSource
 import com.niallmurph.noteapp.screens.NoteScreen
 import com.niallmurph.noteapp.screens.NoteViewModel
 import com.niallmurph.noteapp.ui.theme.NoteAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
